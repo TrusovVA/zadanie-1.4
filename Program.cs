e16476e3ecf09bf80873ca3dd8ce6b460ae8667e
TrusovVA <snegomg@gmail.com>
2014-07-05 11:52:50 +0600

    123

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Ведите первое предложение: ");
+            str = Console.ReadLine();
+            Console.WriteLine("Введите второе предложение: ");
+            str += " " + Console.ReadLine();
+
+            string[] array = str.Split();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "") continue;
+                bool f = true;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] == "" || i == j) continue;
+                    if (array[i] == array[j])
+                    {
+                        array[j] = "";
+                        f = false;
+                    }
+                }
+                if (f) Console.WriteLine(array[i]);
+                array[i] = "";
+            }
+            Console.ReadKey();
+        }
+    }
+}
